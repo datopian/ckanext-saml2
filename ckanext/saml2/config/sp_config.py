9c7c0495244cd3d748e975999bd429e70a71922e
Jovanka Gulicoska <jovanka.gulicoska@datopian.com>
2020-05-21 14:26:08 +0200

    Changes in the sp_config and new sp.xml

--- ckanext/saml2/config/sp_config.py
+++ ckanext/saml2/config/sp_config.py
@@ -5,9 +5,8 @@ from pylons import config
 from saml2 import BINDING_HTTP_REDIRECT
 from saml2.saml import NAME_FORMAT_URI
 
-print('hello sp_config')
 
-saml2_site_url = 'localhost:8080'
+saml2_site_url = 'http://localhost:8080'
 saml2_idp_url = 'https://login.test.max.gov'
 
 base = config.get('saml2.site_url', saml2_site_url)
@@ -22,7 +21,7 @@ CONFIG = {
             'name' : 'CKAN SP',
             'endpoints': {
                 'assertion_consumer_service': [base],
-                'single_logout_service' : [(base + 'slo',
+                'single_logout_service' : [(base + '/slo',
                                             BINDING_HTTP_REDIRECT)],
             },
             # Not sure about this stuff (not in config_max
